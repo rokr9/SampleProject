fa1aac07ca4176f1b5762abeac9814c1f78894d5
rokr9 <rokr9@users.noreply.github.com>
2015-08-06 01:13:30 +0530

    Delete reverse.js

--- reverse.js
+++
@@ -1,6 +0,0 @@
-function reverse(){
-	var a = document.getElementById('line').value;
-	var b = a.split("").reverse().join("");
-	document.getElementById('show').innerHTML = b;
-}
-document.getElementById('myBtn').addEventListener('click', reverse);
